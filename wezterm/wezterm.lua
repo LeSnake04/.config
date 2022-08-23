8f319947b7b50a4af0fa88c35c7baa0c614ca205
LeSnake04 <dev.lesnake@posteo.de>
2022-08-23 17:41:16 +0200

    added wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+
+return {
+	default_prog = { "Informatik/Bash/global/select-zellij-instance" },
+	font_size = 9.3,
+	warn_about_missing_glyphs = false,
+	use_fancy_tab_bar = false,
+}
