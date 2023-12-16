4737781ace41da01bbe3403cea8a92281ca9481e
LeSnake04 <dev.lesnake@posteo.de>
2023-12-16 22:46:02 +0100

    switch to alacritty

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -54,7 +54,7 @@ end
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "wezterm"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "helix"
 editor_cmd = terminal .. " -e " .. editor
 
