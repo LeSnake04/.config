6f2d98a52d4f8486adcaadb4778a1fe454ad8ee5
LeSnake04 <dev.lesnake@posteo.de>
2023-12-16 22:46:22 +0100

    update home_dotfiles script

--- home_dotfiles/setup.sh
+++ home_dotfiles/setup.sh
@@ -1,6 +1,2 @@
-inputs=(ideavimrc)
-outputs=(.ideavimrc)
-
-for i in {0..1}; do
-	ln -s ~/.config/home_dotfiles/"${inputs[i]}" ~/"${outputs[i]}"
-done
+#!/usr/bin/env sh
+ln -s ~/.config/home_dotfiles/ideavimrc ~/.ideavimrc
